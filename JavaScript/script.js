00bfdfcea07c0feadf868c0a46a5a787c996719c
supervova <svvanchikov@yandex.ru>
2019-03-10 22:15:20 +0300

    Это наш первый коммит

---
+++ JavaScript/script.js
@@ -0,0 +1,30 @@
+
+
+
+
+console.log(4/0);
+console.log('string' * 9);
+
+let person = {
+    name: "John", 
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);
+console.log(person["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp']
+
+console.log(arr[0]);
+
+//alert('Hello World');
+
+//let answer = confirm("Are you here");
+
+//console.log(answer);
+
+//let answer = prompt("Are you here", "Да");
+
+console.log(typeof(1));
+
